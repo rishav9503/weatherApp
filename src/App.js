20d15f1882a0765d091bda6c95e53c11f2b8ce3c
Rishav <96rishu.nidhi@gmail.com>
2019-04-16 23:19:27 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {  WiDayCloudy,	 WiFire, WiDayHaze,WiMoonAltWaningCrescent6, WiMoonAltWaningCrescent2,WiDayHail } from 'react-icons/wi';
 import './App.css';
+import {Jumbotron,Row,Col, Container} from 'react-bootstrap'
+
+const rowStyle={
+  height:'370px',
+  border: '1px solid white',
+  background: 'linear-gradient(to top, #30cfd0 0%, #330867 100%)',
+  color:'#ffffff',
+  width: '60%',
+  margin: '0 auto',
+  fontWeight: 'bold',
+  textTransform:'uppercase',
+  fontFamily: 'Arial, Helvetica, sans-serif',
+  padding:'0 auto',
+  borderRadius:'5px'
+ 
+};
+const h1Style={
+  margin:'1%  35% 5% 35%',
+  fontSize:'28px',
+  fontWeight:'bold'
+}
+const row2Style={
+ 
+ 
+  margin: '0 auto',
+  width: '60%',
+  height:'300px'
+
+}
+const h7style={
+  fontSize:'10px',
+  margin:'1%  22% 5% 22%'
+
+}
+
+const date= new Date();
+const tom= date.getTime();
+console.log(tom,"tom")
+const Tommorow= new Date(tom+8640000)
+console.log(Tommorow,99)
+const dayafter=new Date(tom+17280000)
 
 class App extends Component {
+  
+  
   render() {
     return (
+      
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+  
+        <Jumbotron className='jumbo'>
+        
+            <h3 className='jumbo3'>React Weather App</h3>
+            <h6 className="styleh6">You can get the daily forecast of your city here...</h6>
+      </Jumbotron>
+      <Container fluid>
+
+
+        <Row style={rowStyle}>
+        
+          <Col>
+          <h2 style={h1Style}>
+            <span>   Bangalore, India< WiFire size="1.5em"/> </span>
+            <p style={h7style}>{date.toDateString()}</p>
+          </h2>
+          
+          <Row >
+        
+          <Col className="todayDetails temp">
+          32°  <WiDayCloudy size="2.5em" /><br/>
+          HAZE
+          <p className="h8Style">feels like 32°<br/>
+          H -- L 26°<br/>
+          UV Index 0 of 10</p>
+
+          </Col>
+         
+        
+          </Row>
+        
+                  
+          </Col>
+
+        </Row>
+        </Container>
+        <Container fluid>
+
+        <Row style={row2Style}>
+          <Col className='style1 mon'>  Tonight <WiMoonAltWaningCrescent6  size="2.5em"/></Col>
+          <Col className='style1 tue'>{Tommorow.toDateString()} <WiDayHaze size="3em"/></Col>
+          <Col className='style1 wed'>  {Tommorow.toDateString()}  Night <WiMoonAltWaningCrescent2  size="2.5em"/> </Col>
+          <Col className='style1 fri'> {dayafter.toDateString()}<WiDayHail  size="2.5em"/> </Col>
+          
+        </Row>
+    </Container>
+  
+     
       </div>
     );
   }
